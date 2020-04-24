bdbef16b92dbbf8bc05f05876e87a7c39d5da202
Morgan Breth <mbreth1@gmail.com>
2020-04-24 17:26:59 -0500

    Create About.js

---
+++ About.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to my page!</h1>
+        <h3>Purpose: To demonstrate the use of Router in ReactJS</h3>
+      </div>
+    );
+  }
+}
+export default Home;
