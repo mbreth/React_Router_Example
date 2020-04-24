22bce30b98dd4df4addd52f25f742e0db8e39840
Morgan Breth <mbreth1@gmail.com>
2020-04-24 17:26:16 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
