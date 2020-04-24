eb0669913d28a003277eda4142cbbfe115f9a214
Morgan Breth <mbreth1@gmail.com>
2020-04-24 17:27:48 -0500

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Contact</h2>
+        <h3>Here is the link to my Github</h3>
+        <a href="https://github.com/mbreth" target="_blank">
+          Github
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Contact;
